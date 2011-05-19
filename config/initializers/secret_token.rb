87182d81a65f854710a5eb5e40c84a979d69b996
Casey McSpadden <casey@crossriver.com>
2011-05-19 10:46:22 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IphoneApp::Application.config.secret_token = 'c623b9cb06e34782c3c260a603847b33eea289dc3dbbc7b8380780b2d2d55b353176e44ebd3cfe74cf496fecd78e3a524b1382bf4e5ab2a5c121e038c3459f25'
